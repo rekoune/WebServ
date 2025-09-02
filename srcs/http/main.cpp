9b403e759217d385f05ca659ea9bf3784826ac71
AOUKY <aoukyhamza@gmail.com>
2025-09-02 10:27:43 +0100

    parsing the request headers done

--- srcs/http/main.cpp
+++ srcs/http/main.cpp
@@ -1,7 +1,7 @@
 # include "../../includes/Request.hpp"
 
 int main (){
-    std::string reqline ("GET /home HTTP/1.1\r\n hona: test\r\n abde: rekoune\r\n\r\n");
+    std::string reqline ("GET /home HTTP/1.1\r\ndd:    ss\r\nrekoune:  abde  llah\r\n\r\n");
     Request req(reqline);
     HttpStatusCode status = req.parseRequest();
     std::cout << "status = " << status << std::endl;
