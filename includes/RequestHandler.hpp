02684a1cf12c763766bae26761fe1219b3b92de9
rekoune <rekoune123@gmail.com>
2025-09-07 18:58:58 +0100

    Request handler ready for testing

--- includes/RequestHandler.hpp
+++ includes/RequestHandler.hpp
@@ -4,12 +4,27 @@
 # include "Request.hpp"
 # include "config/configStructs.hpp"
 # include "Utils.hpp"
+# include <sys/stat.h>
+# include <unistd.h>
+# include <vector>
+
+enum PathTypes{
+    F,
+    SCRIPT,
+    DIR,
+};
 
 class RequestHandler{
     private:
         Request req;
         ServerConfig server;
-        HttpStatusCode findLocation(std::vector<LocationConfig> locations, std::string reqTarget, LocationConfig& resultLocation);
+        HttpStatusCode  findLocation(std::vector<LocationConfig> locations, std::string reqTarget, LocationConfig& resultLocation);
+        HttpStatusCode  resolveResourceType(std::string& path, PathTypes& pathType, LocationConfig& location);
+        HttpStatusCode  dirHandling(std::string& path, PathTypes& pathType, LocationConfig& location);
+        HttpStatusCode  fileHandling(std::string& path, PathTypes& pathType, LocationConfig& location);
+        bool            isScript(std::string& path, LocationConfig& location);
+        HttpStatusCode  isMethodAllowed(std::vector<std::string> allowedMethods, std::string reqMethod);
+        
     public:
         RequestHandler();
         RequestHandler (const Request& req, const ServerConfig& server);
